0ca6bf4f8add36abb1f09f2484d3e5793ae402b9
DonCastillo <don.qcastillo@yahoo.com>
2021-03-14 01:31:17 -0700

    added footer tabs

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,20 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer } from "./Layouts";
 import Exercises from "./Exercises";
+import { muscles, exercises } from "./../store";
 import "./App.css";
 
 class App extends Component {
+	state = {
+		exercises,
+	};
+
 	render() {
 		return (
 			<Fragment>
 				<Header />
 				<Exercises />
-				<Footer />
+				<Footer muscles={muscles}/>
 			</Fragment>
 		);
 	}
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {Paper, Tabs, Tab} from '@material-ui/core';
+import { Paper, Tabs, Tab } from "@material-ui/core";
 
-
-function Footer(props) {
+function Footer({ muscles }) {
 	return (
-	<Paper>
-		<Tabs
-		  value={0}
-		  indicatorColor="primary"
-		  textColor="primary"
-		  centered
-		>
-		  <Tab label="Item One" />
-		  <Tab label="Item Two" />
-		  <Tab label="Item Three" />
-		</Tabs>
-	  </Paper>
+		<Paper>
+			<Tabs
+				value={0}
+				indicatorColor="primary"
+				textColor="primary"
+				centered
+				muscles={muscles}
+			>
+				{muscles.map((group) => {
+					return <Tab label={group} />;
+				})}
+			</Tabs>
+		</Paper>
 	);
 }
 
